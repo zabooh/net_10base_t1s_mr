5774ed556568a4014738e9282d45c1f526b69b3e
Kevin Bogdon <kevin.bogdon@microchip.com>
2023-01-31 14:27:51 +0100

    Added new states to APP_STATES bases on the pap

---
+++ apps/tcpip_iperf_10base_t1s/firmware/tcpip_iperf_10base_t1s_freertos.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# FreeRTOS configuration
+CND_ARTIFACT_DIR_FreeRTOS=dist/FreeRTOS/production
+CND_ARTIFACT_NAME_FreeRTOS=tcpip_iperf_10base_t1s_freertos.X.production.hex
+CND_ARTIFACT_PATH_FreeRTOS=dist/FreeRTOS/production/tcpip_iperf_10base_t1s_freertos.X.production.hex
